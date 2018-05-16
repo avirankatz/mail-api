87327f81f6e4a45606911b5fd73bf90ca84a72c2
Aviran Katz <aviran.katz@gmail.com>
2018-05-16 21:03:08 +0300

    add https support

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyparser = require('body-parser');
+const fs = require('fs');
 const app = express();
 const cors = require('cors');
+const http = require('http');
+const https = require('https');
+const config = require('./config.json');
+
+if (config.certPath && config.privatekeyPath) {
+    var privateKey = fs.readFileSync(config.privatekeyPath);
+    var certificate = fs.readFileSync(config.certPath);
+    var credentials = { key: privateKey, cert: certificate };
+}
+
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded());
 app.use(cors());
@@ -17,4 +28,10 @@ app.post('/mail', (req, res) => {
     res.json(`Mail sent!\nFrom: ${req.body.from}\nTo: ${req.body.to}`);
 });
 
-app.listen(3000, "0.0.0.0", () => console.log('Mail API listening on port 3000!'));
+// app.listen(3000, "0.0.0.0", () => console.log('Mail API listening on port 3000!'));
+http.createServer(app).listen(3000);
+if (credentials != null)
+    https.createServer({
+        key: privateKey,
+        cert: certificate
+    }, app).listen(8443);
