ffb86ef700a9f7c3358e434f7fe9043c25d8953b
Aviran Katz <aviran.katz@gmail.com>
2018-05-30 16:26:04 +0300

    add send sms api

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 const cors = require('cors');
 const http = require('http');
 const https = require('https');
+const request = require('request');
 const config = require('./config.json');
 
 if (config.certPath && config.privatekeyPath) {
@@ -26,6 +27,16 @@ app.post('/mail', (req, res) => {
         html: req.body.html
     });
     res.json(`Mail sent!\nFrom: ${req.body.from}\nTo: ${req.body.to}`);
+});
+
+app.get('/sms', (req, res) => {
+    let messageString = "\xD7\x9C\xD7\x99\xD7\xA0\xD7\xA7\x20\xD7\x9C\xD7\x94\xD7\x95\xD7\xA8\xD7\x93\xD7\xAA\x20\x50\x61\x79\x42\x6F\x78\x0A\x68\x74\x74\x70\x73\x3A\x2F\x2F\x70\x61\x79\x62\x6F\x78\x63\x61\x6D\x70\x61\x69\x67\x6E\x2E\x63\x6F\x6D\x2F\x64\x6F\x77\x6E\x6C\x6F\x61\x64\x2E\x68\x74\x6D\x6C";
+    let url = `https://api.inforu.co.il/inforufrontend/WebInterface/SendMessageByNumber.aspx?UserName=${config.smsUser}&Password=${config.smsPassword}&SenderCellNumber=PayBox&CellNumber=${req.query.number}&MessageString=${messageString}`;
+    request.get(url, (err, rsp, body) => {
+        if (body == "1") res.status(200);
+        else res.status(500);
+        res.json(res.statusCode);
+    });
 });
 
 // app.listen(3000, "0.0.0.0", () => console.log('Mail API listening on port 3000!'));
